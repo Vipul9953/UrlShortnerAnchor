7d033b513bc0c8bc4afc3259c8a1567180a04524
vipul108coder <112881501+vipul108coder@users.noreply.github.com>
2024-02-18 17:37:19 +0530

    Your commit message herse

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,65 @@
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const app = express();
+// const errorMiddleware = require("./middleware/error");
+// const cookieParser = require("cookie-parser");
+// const cors = require('cors');
+// const path = require('path')
+// // Enable CORS with the appropriate origin
+// app.use(cors({
+//     origin: ["https://url-shortner-anchor-mern-dev.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// }));
+
+
+// //midlleware
+// //using middleware
+// app.use(express.json());
+// app.use(cookieParser());
+
+// dotenv.config({path:"backend/config/config.env"})
+
+// //database connected
+// const dataBase = require('./config/database');
+// dataBase();
+
+
+
+// //routing
+// const URL = require("./routes/urlRoute");
+// const USER = require("./routes/userRoute");
+// app.use("/api/v1", URL);
+// app.use("/api/v1", USER);
+
+
+
+
+
+// //Middleware for Errors
+// app.use(errorMiddleware);
+
+
+
+
+// app.listen(process.env.PORT || 4000, ()=>{
+//     console.log(`server is working at ${process.env.PORT}`);
+// })
+
+
+// api/index.js
 const express = require('express');
-const dotenv = require('dotenv');
-const app = express();
-const errorMiddleware = require("./middleware/error");
-const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path')
-// Enable CORS with the appropriate origin
-app.use(cors({
-    origin: ["https://url-shortner-anchor-mern-dev.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-//midlleware
-//using middleware
-app.use(express.json());
-app.use(cookieParser());
+// Your routes go here
+app.get('/api/v1/yourEndpoint', (req, res) => {
+  // Handle your endpoint logic
+  res.json({ message: 'Hello from Vercel serverless function!' });
+});
 
-dotenv.config({path:"backend/config/config.env"})
-
-//database connected
-const dataBase = require('./config/database');
-dataBase();
-
-
-
-//routing
-const URL = require("./routes/urlRoute");
-const USER = require("./routes/userRoute");
-app.use("/api/v1", URL);
-app.use("/api/v1", USER);
-
-
-
-
-
-//Middleware for Errors
-app.use(errorMiddleware);
-
-
-
-
-app.listen(process.env.NEXT_PUBLIC_API_URL || process.env.PORT || 4000, ()=>{
-    console.log(`server is working at ${process.env.PORT}`);
-})
+module.exports = app;
