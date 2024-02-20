f4fd90c0404963aba21d0e12b6feb723d13c2263
Vipul Kumar <87822201+Vipul9953@users.noreply.github.com>
2024-02-20 22:47:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const app = express();
-// const errorMiddleware = require("./middleware/error");
-// const cookieParser = require("cookie-parser");
-// const cors = require('cors');
-// const path = require('path')
-// // Enable CORS with the appropriate origin
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
+const path = require('path')
+// Enable CORS with the appropriate origin
+app.use(cors());
+
+
+//midlleware
+//using middleware
+app.use(express.json());
+app.use(cookieParser());
+
+dotenv.config({path:"backend/config/config.env"})
+
+//database connected
+const dataBase = require('./config/database');
+dataBase();
 
 
 
-// //midlleware
-// //using middleware
-// app.use(express.json());
-// app.use(cookieParser());
-
-// dotenv.config({path:"backend/config/config.env"})
-
-// //database connected
-// const dataBase = require('./config/database');
-// dataBase();
-
-
-
-// //routing
-// const URL = require("./routes/urlRoute");
-// const USER = require("./routes/userRoute");
-// app.use("/api/v1", URL);
-// app.use("/api/v1", USER);
-
-
-
-
-
-// //Middleware for Errors
-// app.use(errorMiddleware);
+//routing
+const URL = require("./routes/urlRoute");
+const USER = require("./routes/userRoute");
+app.use("/api/v1", URL);
+app.use("/api/v1", USER);
 
 
 
 
-// app.listen(process.env.PORT || 4000, ()=>{
-//     console.log(`server is working at ${process.env.PORT}`);
-// })
+
+//Middleware for Errors
+app.use(errorMiddleware);
+
+
+
+
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log(`server is working at ${process.env.PORT}`);
+})
 
 
 // api/index.js
