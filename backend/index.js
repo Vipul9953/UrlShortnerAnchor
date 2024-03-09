ecfb3f0752f4f0ece48316cf260c0e594958a781
Vipul Kumar <87822201+Vipul9953@users.noreply.github.com>
2024-03-09 10:30:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
-{
-  // "name": "anchors",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "dev": "nodemon backend/index.js"
-    },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "axios": "^1.6.7",
-    "nodemon": "^3.1.0",
-    "bcrypt": "^5.1.1",
-    "bcryptjs": "^2.4.3",
-    "cookie-parser": "^1.4.6",
-    "cors": "^2.8.5",
-    "crypto": "^1.0.1",
-    "dotenv": "^16.4.1",
-    "express": "^4.18.2",
-    "jsonwebtoken": "^9.0.2",
-    "mongoose": "^8.1.1",
-    "nodemailer": "^6.9.9",
-    "react": "^17.0.0",
-    "react-dom": "^17.0.0", 
-    "shortid": "^2.2.16",
-    "validator": "^13.11.0"
-  }
-}
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser"); 
+const  connectDatabase = require("./config/database"); 
+const dotenv = require('dotenv');
+require("dotenv").config()
+
+// Using Middlewares
+app.use(express.json());
+// app.use(express.urlencoded());
+app.use(cookieParser());
+
+// Importing Routes
+const user = require("./routes/userRoute");
+const url = require("./routes/urlRoute");
+
+// Using Routes
+app.use("/api/v1", user);
+app.use("/api/v1", url);
+ 
+
+connectDatabase();
+ 
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
+module.exports = app;
