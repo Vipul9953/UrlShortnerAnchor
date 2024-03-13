4236fa0a8fad4ff2010e49f3b80576f0566d56b8
Vipul Kumar <87822201+Vipul9953@users.noreply.github.com>
2024-03-13 12:26:26 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(login);
 router.route("/logout").get(logoutUser);
-router.route("/my/urls").get( getMyUrls);
+router.route("/my/urls").get(isAuthenticatedUser, getMyUrls);
 router.route("/forgot/password").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 module.exports = router;
